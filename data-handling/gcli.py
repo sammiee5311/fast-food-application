780ccd498cf57becb53b12c8a1845760a37da23b
sammiee5311 <sammiee5311@gmail.com>
2021-12-10 14:12:40 +0900

    [Deployment] Update error exception

--- data-handling/gcli.py
+++ data-handling/gcli.py
@@ -1,11 +1,18 @@
 import click
 import requests
+from requests.exceptions import ConnectionError
 
 from utils import get_schema
 
 FEATURES = list(get_schema().keys())
 HOST = "localhost"
 PORT = "8080"
+
+
+class DataNotExist(AttributeError):
+    def __init__(self, message="Data does not exist. Please Check your data."):
+        self.message = message
+        super().__init__(self.message)
 
 
 @click.group()
@@ -17,21 +24,25 @@ def cli():
 @click.option("--data", help="feature data")
 @click.option("--host", default=f"http://{HOST}:{PORT}/predict", help="host")
 def predict_value(data: str, host: str):
-    _data = list(map(lambda feature: feature.strip(), data.split(",")))
-
     try:
+        if not data:
+            raise DataNotExist
+        _data = list(map(lambda feature: feature.strip(), data.split(",")))
+
         payload = {key: val for key, val in zip(FEATURES, _data)}
-    except ValueError as error:
+
+        response_data = requests.post(url=host, json=payload)
+    except (ValueError, DataNotExist, ConnectionError) as error:
         click.echo(click.style(f"{error}", bg="red", fg="white"))
         return
 
     click.echo(click.style(f"Querying host {host} with data: {payload}", bg="black", fg="white"))
-    result = requests.post(url=host, json=payload)
 
-    if "error" in result.json():
-        click.echo(click.style(f"Result : {result.json()}", bg="red", fg="white"))
-    else:
-        click.echo(click.style(f"Result : {result.json()}", bg="green", fg="black"))
+    result = response_data.json()
+
+    bg, fg = ("green", "black") if "error" in result else ("red", "white")
+
+    click.echo(click.style(f"Result : {result}", bg=bg, fg=fg))
 
 
 if __name__ == "__main__":
