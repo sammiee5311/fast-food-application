7f9b4dee8e8f978221b11bb73121f9369e088d48
sammiee5311 <sammiee5311@gmail.com>
2022-05-20 21:02:08 +0900

    [Django & CI] Fix url name

--- django/core/urls.py
+++ django/core/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("home.urls", namespace="restaurant")),
+    path("", include("restaurant.urls", namespace="restaurant")),
     path("api/v0/", include("api.urls", namespace="api")),
 ]
